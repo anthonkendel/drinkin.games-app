30a25cfba0b583978dfebd88432cea9e143b8592
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-09-26 21:25:43 +0200

    Update configurations
    * Minor updates to eslint
    * Add custom prettier config
    * Fix tests not working
    * Fix errors and warnings present after
    changes in configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/recommended', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/html-closing-bracket-newline': [
+      'warning',
+      {
+        multiline: 'always',
+      },
+    ],
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'always',
+      },
+    ],
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint',
+  },
 };
