66d91fc329eaba5feafe05dad7848dd4675ec9ae
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-10-02 20:17:48 +0200

    Add basic sidebar menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view />
+    <div class="columns">
+      <div class="column is-narrow">
+        <SidebarMenu />
+      </div>
+      <div class="column router-view">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar';
+import Navbar from '@/components/Navbar';
+import SidebarMenu from '@/components/SidebarMenu';
 
 export default {
   name: 'DrinkinGamesApp',
-  components: { Navbar },
+  components: { Navbar, SidebarMenu },
 };
 </script>
+
+<style lang="scss" scoped>
+.router-view {
+  padding: 1.5rem;
+}
+</style>
