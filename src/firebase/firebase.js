b0a56ac54192331ef965220bdd20bc0f868fd33c
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-10-02 19:29:57 +0200

    Setup firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+export const config = {
+  apiKey: 'AIzaSyAb4XVY9MqYmCeCf-oCBVj8OsCLsK7p7g0',
+  authDomain: 'drinkin-games-81ea4.firebaseapp.com',
+  databaseURL: 'https://drinkin-games-81ea4.firebaseio.com',
+  projectId: 'drinkin-games-81ea4',
+  storageBucket: '',
+  messagingSenderId: '1078934947548',
+};
+firebase.initializeApp(config);
+
+export const firestore = firebase.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
+export default {
+  config,
+  firestore,
+};
