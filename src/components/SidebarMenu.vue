24a3d377ecc0707b067127bc50ceef4385289e47
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-10-15 20:25:16 +0200

    Update SidebarMenu to show games from store

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,43 +1,44 @@
 <template>
   <aside class="menu">
-    <p class="menu-label">
-      General
-    </p>
-    <ul class="menu-list">
-      <li><a>Dashboard</a></li>
-      <li><a>Customers</a></li>
-    </ul>
-    <p class="menu-label">
-      Administration
-    </p>
-    <ul class="menu-list">
-      <li><a>Team Settings</a></li>
-      <li>
-        <a class="is-active">Manage Your Team</a>
-        <ul>
-          <li><a>Members</a></li>
-          <li><a>Plugins</a></li>
-          <li><a>Add a member</a></li>
-        </ul>
+    <p class="menu-label">Top rated games</p>
+    <ul
+      v-if="topRatedGames.length > 0"
+      class="menu-list"
+    >
+      <li
+        v-for="(game, index) in topRatedGames"
+        :key="index"
+      >
+        <a>{{ game.name }}</a>
       </li>
-      <li><a>Invitations</a></li>
-      <li><a>Cloud Storage Environment Settings</a></li>
-      <li><a>Authentication</a></li>
     </ul>
+
     <p class="menu-label">
-      Transactions
+      New games
     </p>
+
     <ul class="menu-list">
-      <li><a>Payments</a></li>
-      <li><a>Transfers</a></li>
-      <li><a>Balance</a></li>
+      <li
+        v-for="(game, index) in newGames"
+        :key="index"
+      >
+        <a>{{ game.name }}</a>
+      </li>
     </ul>
   </aside>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'SidebarMenu',
+  computed: {
+    ...mapGetters({
+      topRatedGames: 'getTopRatedGames',
+      newGames: 'getNewGames',
+    }),
+  },
 };
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -3,20 +3,59 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+class Game {
+  constructor(name, rating, created) {
+    this.name = name;
+    this.rating = rating;
+    this.created = created;
+  }
+}
+
+function getAverage(array) {
+  return array.reduce((p, c) => p + c, 0) / array.length;
+}
+
 export default new Vuex.Store({
   state: {
     signUp: false,
     authentication: false,
     noAuthentication: false,
+
+    games: [
+      new Game('Ring of fire', [1, 4, 5], '2018-01-01'),
+      new Game('Beer game', [1, 2, 3], '2018-02-02'),
+      new Game('Kings cup', [1, 4, 5], '2018-03-03'),
+      new Game('Never have I ever', [1, 4, 5], '2018-04-04'),
+      new Game('Flip cup', [1, 4, 5], '2018-05-05'),
+      new Game('Buzz', [1, 4, 5], '2018-06-06'),
+      new Game('Drunk jenga', [5, 5, 5], '2017-07-07'),
+    ],
   },
+  getters: {
+    getTopRatedGames(state) {
+      const games = state.games.slice(0).map(game => ({ ...game, averageRating: getAverage(game.rating) }));
+      games.sort((a, b) => a.averageRating - b.averageRating).reverse();
+      return games.slice(0, 5);
+    },
+    getNewGames(state) {
+      const games = state.games.slice(0);
+      games.sort((a, b) => new Date(a.created) - new Date(b.created)).reverse();
+      return games.slice(0, 5);
+    },
+  },
+
   mutations: {
     setSignUp: (state, signUp) => (state.signUp = signUp),
     setAuthentication: (state, authentication) => (state.authentication = authentication),
     setNoAuthentication: (state, noAuthentication) => (state.noAuthentication = noAuthentication),
+
+    setGames: (state, games) => (state.games = [...games]),
   },
   actions: {
     setSignUp: ({ commit }, signUp) => commit('setSignUp', signUp),
     setAuthentication: ({ commit }, authentication) => commit('setAuthentication', authentication),
     setNoAuthentication: ({ commit }, noAuthentication) => commit('setNoAuthentication', noAuthentication),
+
+    setGames: ({ commit }, games) => commit('setGames', games),
   },
 });
