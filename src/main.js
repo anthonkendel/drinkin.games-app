fe75e90c080ac32077502f8f65e0f11a8ee32eb4
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-10-23 20:45:49 +0200

    Add Buefy as dependency

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import '@/firebase/firebase';
 import '@/registerServiceWorker';
 import router from '@/router/router';
 import store from '@/store/store';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 import 'bulma';
 import Vue from 'vue';
+
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
