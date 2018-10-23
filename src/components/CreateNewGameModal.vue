967775e165291b9233c5a8400353cb4695ef8687
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-10-23 20:47:21 +0200

    Add create new game modal
    The component has no inputs or logic as of now.
    The component will be implemented in a later
    commit.

---
+++ src/components/CreateNewGameModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Create new game</p>
+      </header>
+      <section class="modal-card-body">
+        Modal card body
+      </section>
+      <footer class="modal-card-foot">
+        <button
+          type="button"
+          class="button is-warning"
+          @click="$parent.close()"
+        >
+          Cancel
+        </button>
+        <button class="button is-primary">Create</button>
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'CreateNewGameModal',
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
+    <div>
+      <button
+        class="button is-primary"
+        @click="createNewGame"
+      >Create new game</button>
+    </div>
     <GamesListing />
   </div>
 </template>
 
 <script>
+import CreateNewGameModal from '@/components/CreateNewGameModal';
 import GamesListing from '@/components/GamesListing';
 
 export default {
   name: 'Home',
   components: { GamesListing },
+  methods: {
+    createNewGame() {
+      this.$modal.open({
+        parent: this,
+        component: CreateNewGameModal,
+        hasModalCard: true,
+      });
+    },
+  },
 };
 </script>
