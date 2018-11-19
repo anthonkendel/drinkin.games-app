3449f6a85ea2fd56c137648b0b38c9032ca45651
Anthon Holmqvist <anthon.holmqvist@softhouse.se>
2018-11-19 21:36:29 +0100

    Add loading indicator to SidebarGames

--- src/components/SidebarGames.vue
+++ src/components/SidebarGames.vue
@@ -1,30 +1,51 @@
 <template>
   <aside class="menu is-hidden-touch">
-    <p class="menu-label">Top rated games</p>
-    <ul
-      v-if="topRatedGames.length > 0"
-      class="menu-list"
-    >
-      <li
-        v-for="(game, index) in topRatedGames"
-        :key="index"
-      >
-        <a>{{ game.name }}</a>
-      </li>
-    </ul>
+    <div class="menu-container">
+      <template v-if="totalGames > 0">
+        <p class="menu-label">Top rated games</p>
+        <ul
+          v-if="topRatedGames.length > 0"
+          class="menu-list"
+        >
+          <li
+            v-for="(game, index) in topRatedGames"
+            :key="index"
+          >
+            <a>{{ game.name }}</a>
+          </li>
+        </ul>
 
-    <p class="menu-label">
-      New games
-    </p>
+        <p class="menu-label">
+          New games
+        </p>
+        <div class="menu-container">
+          <ul
+            v-if="newGames.length > 0"
+            class="menu-list"
+          >
+            <li
+              v-for="(game, index) in newGames"
+              :key="index"
+            >
+              <a>{{ game.name }}</a>
+            </li>
+          </ul>
 
-    <ul class="menu-list">
-      <li
-        v-for="(game, index) in newGames"
-        :key="index"
-      >
-        <a>{{ game.name }}</a>
-      </li>
-    </ul>
+          <b-loading
+            v-if="newGames.length < 1"
+            :is-full-page="false"
+            active
+          />
+        </div>
+      </template>
+      <template v-else>
+        <b-loading
+          :is-full-page="false"
+          class="loading-indicator"
+          active
+        />
+      </template>
+    </div>
   </aside>
 </template>
 
@@ -38,6 +59,7 @@ export default {
       topRatedGames: 'getTopRatedGames',
       newGames: 'getNewGames',
     }),
+    totalGames: vm => vm.topRatedGames.length + vm.newGames.length,
   },
   created() {
     this.loadGames();
@@ -51,5 +73,10 @@ export default {
 <style lang="scss" scoped>
 .menu {
   padding: 1.5rem;
+}
+
+.menu-container {
+  position: relative;
+  height: 100vh;
 }
 </style>
