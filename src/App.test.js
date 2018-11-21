433e8db5ad0bc47a4e1c63f0bff0bce6e045f7ff
Anthon Holmqvist <anthon.holmqvist@softhouse.se>
2018-11-21 19:34:33 +0100

    Update failing snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,17 @@
 import App from '@/App';
 import router from '@/router/router';
 import store from '@/store/store';
-import { mount } from '@vue/test-utils';
+import Buefy from 'buefy';
+import { mount, createLocalVue } from '@vue/test-utils';
 
 describe('App.vue', () => {
   it('should match snapshot', () => {
+    const localVue = createLocalVue();
+    localVue.use(Buefy, { defaultIconPack: 'fa' });
     const wrapper = mount(App, {
       router,
       store,
+      localVue,
     });
     expect(wrapper.element).toMatchSnapshot();
   });
