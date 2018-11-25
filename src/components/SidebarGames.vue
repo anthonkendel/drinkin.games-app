1d6d25a97f948e2e61fe8ea745e3638e79688e12
Anthon Holmqvist <anthon.kendel@gmail.com>
2018-11-25 15:21:53 +0100

    Remove loading indicator in SidebarGames

--- src/components/SidebarGames.vue
+++ src/components/SidebarGames.vue
@@ -1,14 +1,34 @@
 <template>
   <aside class="menu">
     <div class="menu-container">
-      <template v-if="totalGames > 0">
-        <p class="menu-label">Top rated games</p>
+      <p class="menu-label">Top rated games</p>
+      <ul
+        v-if="topRatedGames.length > 0"
+        class="menu-list"
+      >
+        <li
+          v-for="game in topRatedGames"
+          :key="game.id"
+        >
+          <router-link
+            :to="{ name: 'Details', params: { id: game.id }}"
+            exact-active-class="is-active"
+          >
+            {{ game.name }}
+          </router-link>
+        </li>
+      </ul>
+
+      <p class="menu-label">
+        New games
+      </p>
+      <div class="menu-container">
         <ul
-          v-if="topRatedGames.length > 0"
+          v-if="newGames.length > 0"
           class="menu-list"
         >
           <li
-            v-for="game in topRatedGames"
+            v-for="game in newGames"
             :key="game.id"
           >
             <router-link
@@ -20,41 +40,12 @@
           </li>
         </ul>
 
-        <p class="menu-label">
-          New games
-        </p>
-        <div class="menu-container">
-          <ul
-            v-if="newGames.length > 0"
-            class="menu-list"
-          >
-            <li
-              v-for="game in newGames"
-              :key="game.id"
-            >
-              <router-link
-                :to="{ name: 'Details', params: { id: game.id }}"
-                exact-active-class="is-active"
-              >
-                {{ game.name }}
-              </router-link>
-            </li>
-          </ul>
-
-          <b-loading
-            v-if="newGames.length < 1"
-            :is-full-page="false"
-            active
-          />
-        </div>
-      </template>
-      <template v-else>
         <b-loading
+          v-if="newGames.length < 1"
           :is-full-page="false"
-          class="b-loading"
           active
         />
-      </template>
+      </div>
     </div>
   </aside>
 </template>
